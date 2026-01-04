30fd55d00be44aeedbf805f7881523bf1b3f6ab7
Dan013577947 <93627888+Dan013577947@users.noreply.github.com>
2026-01-04 15:11:05 +0800

    made like text-blue-600 when clicked, also saved the status in localstorage

---
+++ src/pages/home/Contents.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import type { ContentType } from "../../interface/Contents";
+
+interface Content {
+  content: ContentType;
+}
+
+
+export default function ContentsBody({ content }: Content) {
+  const [likeBool, setLikeBool] = useState<boolean | null>(()=>{
+    const savedItem = localStorage.getItem(`likeBool${content.id}`)
+    return savedItem 
+    ? JSON.parse(savedItem)
+    : null
+  })
+
+  const handleLike = () => {
+    setLikeBool(prev => {
+      localStorage.setItem(`likeBool${content.id}`, JSON.stringify(!prev))
+      return !prev
+    })
+  }
+  return (
+    <div className="mx-auto   max-w-sm sm:max-w-lg lg:max-w-3xl mb-5 font-[Roboto,sans-serif] p-4 bg-white shadow-[0_0_5px_rgba(0,0,0,0.1)]">
+      <div className="text-xl font-bold pb-5">{content.title}</div>
+      <div>{content.body}</div>
+      <div className="flex items-center italic text-sm text-gray-300">
+        <div className="pr-1">tags:</div>
+        {content.tags.map(tag => {
+          return (
+            <div key={crypto.randomUUID()} className="mr-2 cursor-pointer hover:text-blue-300">
+              {tag}
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex justify-evenly">
+        {likeBool
+          ? <div className="cursor-pointer text-blue-600" onClick={() => handleLike()}>Like</div>
+          : <div className="cursor-pointer" onClick={() => handleLike()}>Like</div>
+        }
+
+        <div className="cursor-pointer">Comment</div>
+        <div className="cursor-pointer">Share</div>
+      </div>
+
+
+    </div>
+  );
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import Header from "../../components/Header"
 import type { Contents, ContentType } from "../../interface/Contents";
+import ContentsBody from "./Contents";
 
 interface Home {
   contents: Contents | undefined;
@@ -11,29 +12,13 @@ export default function Home({ contents }: Home) {
       <Header />
       <div className="pt-20 bg-gray-100">
         {contents?.posts.map((content: ContentType) => {
-          console.log(content)
           return (
-            <div key={content.id} className="mx-auto max-w-sm sm:max-w-lg lg:max-w-3xl mb-5 font-[Roboto,sans-serif] p-4 bg-white shadow-[0_0_5px_rgba(0,0,0,0.1)]">
-              <div className="text-xl font-bold pb-5">{content.title}</div>
-              <div>{content.body}</div>
-              <div className="flex items-center italic text-sm text-gray-300">
-                <div className="pr-1">tags:</div>
-                {content.tags.map(tag=>{
-                  return(
-                    <div className="mr-2 cursor-pointer hover:text-blue-300">
-                      {tag}
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="flex justify-evenly">
-                <div className="cursor-pointer">Like</div>
-                <div className="cursor-pointer">Comment</div>
-                <div className="cursor-pointer">Share</div>
-              </div>
-              
-
+            <div key={content.id}>
+              <ContentsBody
+                content={content}
+              />
             </div>
+
           );
         })}
       </div>
